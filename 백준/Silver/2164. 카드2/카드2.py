a8bb5b51e72a72021d311fb7e09b9fffb5bf4657
git_hoon <55564114+hoon-git@users.noreply.github.com>
2022-05-22 14:03:58 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 49252 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque 
+
+n = int(input())
+
+queue = deque()
+
+for i in range(1,n+1):
+  queue.append(i)
+
+while(len(queue) > 1):
+  queue.popleft()
+  queue.append(queue.popleft())
+
+print("{}".format(queue[0]))
