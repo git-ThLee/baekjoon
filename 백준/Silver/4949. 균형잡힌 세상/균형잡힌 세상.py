31e92fd86064e2e0b2367186402156637006d9cc
ThLee <55564114+git-ThLee@users.noreply.github.com>
2022-05-24 14:05:57 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 244 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,12 +1,16 @@
+# 문단 
 Paragraph = ''
+# 문단 모으기
 while 1:
     i = str(input())
     Paragraph += i
     if i == '.':
         break
-        
+# 
 for j in range(Paragraph.count('.')-1):
+    # 0은 인덱스 에러 방지용
     stack1 = [0] 
+    # 문장 
     sentence =  Paragraph[0:Paragraph.index('.')]
     Paragraph = Paragraph[Paragraph.index('.')+1:]
     for k in sentence:
