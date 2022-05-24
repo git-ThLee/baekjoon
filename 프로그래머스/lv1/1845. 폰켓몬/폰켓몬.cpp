43797aab88dbfe8825dd1fa7f4b095b48f6ea9ff
ThLee <55564114+git-ThLee@users.noreply.github.com>
2022-05-24 15:56:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.29 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include<iostream>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int a = nums.size() / 2 ;
+    
+    sort(nums.begin() , nums.end());
+    nums.erase(unique(nums.begin() , nums.end()) , nums.end());
+
+    if(nums.size() <= a){
+        answer = nums.size() ;
+    }else{
+        answer = a;
+    }
+    return answer;
+}
