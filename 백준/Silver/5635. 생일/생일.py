72d58dd91aa525235fe0edfd88221351507efe86
ThLee <55564114+git-ThLee@users.noreply.github.com>
2023-05-27 15:25:29 +0900

    [Silver V] Title: 생일, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -1,13 +1,13 @@
-case = int(input())
-peoples = dict()
-for _ in range(case):
-  name , dd , mm , yy = map(str,input().split())
-  if len(mm) == 1 :
-    mm = '0' + mm
-  if len(dd) == 1 :
-    dd = '0' + dd
-  y_d_m = f'{yy}-{mm}-{dd}'
-  peoples[name] = y_d_m
-ans = sorted(peoples.items(), key=lambda x : x[1])
-print(ans[-1][0])
-print(ans[0][0])
+n = int(input())
+mans = list()
+for _ in range(n):
+  name , day , month , year = map(str,input().split())
+  mans.append({
+      'name' : name,
+      'year' : int(year),
+      'month' : int(month),
+      'day' : int(day),
+  })
+  mans = sorted(mans, key = lambda x : (x['year'],x['month'],x['day']))
+print(mans[-1]['name'])
+print(mans[0]['name'])
